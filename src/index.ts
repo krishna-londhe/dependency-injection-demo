0b6055d3f4252507ca64789a5f09cea7774941ce
Dattatraya-Londhe <londhedn@gmail.com>
2023-07-25 12:08:31 +0530

    Added comments

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
+//without dependency injection 
+
 import { StudentController } from "./controller/demo1";
 import { StudentService } from "./service/student.service";
 
 const serviceInstance = new StudentService();
 
+// student controller is depends on student service class
+// Manually created instance for StduentService class and
+// pass that instance to StudentController 
 const controllerInstance = new StudentController(serviceInstance);
 
 controllerInstance.display();
