1c772ee80d23448dbf1206573e0a83e60eafa9f2
Dattatraya-Londhe <londhedn@gmail.com>
2023-07-25 12:57:57 +0530

    updated for @injectable @singleton

--- src/controller/demo1.ts
+++ src/controller/demo1.ts
@@ -1,12 +1,30 @@
+import { injectable, singleton } from "tsyringe";
 import { StudentService } from "../service/student.service";
 
+//identify the dependency through constructor parameters and 
+//auto-inject them during the runtime.
+
+// @injectable()
+// export class StudentController {
+//   constructor(private service: StudentService) {
+    
+//   }
+//   display(){
+//     console.log('Calling from Student Controller')
+//     this.service.displayStudent();
+//   }
+// }
+
+////////////////////////////////////////////////////////////
+@singleton()
 export class StudentController {
   constructor(private service: StudentService) {
     
   }
-
   display(){
     console.log('Calling from Student Controller')
     this.service.displayStudent();
   }
 }
+
+////////////////////////////////////////////////////////////
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
-//without dependency injection 
-
+//with dependency injection 
+import 'reflect-metadata'
+import { container } from 'tsyringe';
 import { StudentController } from "./controller/demo1";
 import { StudentService } from "./service/student.service";
 
-const serviceInstance = new StudentService();
+// const serviceInstance = new StudentService();
 
-// student controller is depends on student service class
-// Manually created instance for StduentService class and
-// pass that instance to StudentController 
-const controllerInstance = new StudentController(serviceInstance);
+// const controllerInstance = new StudentController(serviceInstance);
+
+const controllerInstance = container.resolve(StudentController);
+//it return the instance of StudentController with auto-injected dependecy if any
 
 controllerInstance.display();
 
